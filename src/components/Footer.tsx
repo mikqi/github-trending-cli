4f49c755aa7143cc43ed9782a0ab84fb8f152b67
Teno Siswono <tennosys@gmail.com>
2019-10-28 11:02:36 +0700

    fix(ux): input language, date, and list view

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,27 @@ import { Box, Text, Color } from 'ink'
 import { AppContext } from '../AppContext'
 
 const Footer = () => {
-  const { language } = useContext(AppContext)
+  const { language, dateRange } = useContext(AppContext)
   return (
-    <Box>
-      <Text>
-        This is your language <Color greenBright>{language}</Color>
-      </Text>
+    <Box flexDirection="column">
+      {language ? (
+        <Box>
+          <Text>
+            This is your language <Color greenBright>{language}</Color>
+          </Text>
+        </Box>
+      ) : (
+        undefined
+      )}
+      {dateRange ? (
+        <Box>
+          <Text>
+            Date range <Color greenBright>{dateRange}</Color>
+          </Text>
+        </Box>
+      ) : (
+        undefined
+      )}
     </Box>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,25 +4,36 @@ import TextInput from 'ink-text-input'
 import { AppContext } from '../AppContext'
 
 const Header = () => {
-  const { query, dispatch } = useContext(AppContext)
+  const { query, dispatch, activePage } = useContext(AppContext)
   const handleChange = (val: string) => {
     dispatch({
       type: 'FILTER_LANGUAGE',
       payload: val,
     })
   }
-  return (
-    <Box>
-      <Text>
-        Search your languages:{' '}
-        <TextInput
-          placeholder="ex. JavaScript"
-          value={query}
-          onChange={handleChange}
-        />
-      </Text>
-    </Box>
-  )
+  let HeaderChild
+  switch (activePage) {
+    case 'searchLanguage':
+      HeaderChild = (
+        <Text>
+          Search your languages:{' '}
+          <TextInput
+            placeholder="ex. JavaScript"
+            value={query}
+            onChange={handleChange}
+          />
+        </Text>
+      )
+      break
+
+    case 'setDateRange':
+      HeaderChild = <Text>Pick date range:</Text>
+      break
+
+    default:
+      break
+  }
+  return <Box>{HeaderChild}</Box>
 }
 
 export default Header
